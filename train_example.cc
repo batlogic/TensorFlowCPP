b3cc7148ded51ae96a5556c5d27a794fa07cc0a7
Dror Meirovich <dror@drorm.com>
2017-06-28 14:34:20 +0300

    Train example in tensorflow (Not working yet)

---
+++ train_example.cc
@@ -0,0 +1,48 @@
+#include <tensorflow/c/c_api.h>
+#include "tensorflow/cc/client/client_session.h"
+#include "tensorflow/cc/ops/standard_ops.h"
+#include "tensorflow/core/framework/tensor.h"
+
+namespace tf {
+    using namespace ::tensorflow;
+    using namespace ::tensorflow::ops;
+
+    tf::Output FullyConnectedLinear(const tf::Scope& scope, tf::Input inp, const int sizeOutput) {
+        const int size_input = inp.tensor().shape().dim_size(1); // dim_size(1) == number of columns
+        auto tf_dt   = inp.data_type();
+        auto weights = tf::Variable(scope, {size_input,sizeOutput}, tf_dt);
+        tf::Assign(scope, weights, tf::TruncatedNormal(scope, weights, tf_dt));
+        auto biases  = tf::Variable(scope, {sizeOutput}, tf_dt);
+        tf::Assign(scope, biases, tf::ZerosLike(scope, biases));
+        return tf::Add (scope, tf::MatMul(scope, inp, weights), biases);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    const auto nin     = 300; // number of inputs features
+    const auto nhidden = 10; // number of hidden units
+    const auto nout    = 10; // number of outputs
+    const auto alpha   = 0.01; // learning rate
+    tf::Scope    scope = tf::Scope::NewRootScope();
+    tf::DataType tf_dt = tf::DT_DOUBLE;
+    auto inputLinesPlaceHolder  = tf::Placeholder(scope.WithOpName("input"),  tf_dt, tf::Placeholder::Shape({-1,nin}) /*shape = (None,nin)*/);
+    auto outputLinesPlaceHolder = tf::Placeholder(scope.WithOpName("output"), tf_dt, tf::Placeholder::Shape({-1,nout}) /*shape = (None,nout)*/);
+    // Layer 1: Fully Connected. Input = nin. Output = nhidden.
+    auto fc1 = tf::FullyConnectedLinear(scope,inputLinesPlaceHolder,nhidden);
+
+    // activation of layer 1
+    auto tanh1   = tf::Tanh(scope,fc1);
+
+    // Layer 2: Fully Connected. Input = nhidden. Output = nout.
+    auto fc2 = tf::FullyConnectedLinear(scope,tanh1,nout);
+    // what should be the axis?
+    auto cost_function = tf::Mean(scope,tf::SquaredDifference(scope,fc2,outputLinesPlaceHolder),1/*axis*/);
+    auto train_operation = tf::ApplyAdam(scope.WithOpName("train"),cost_function,
+                                         0/*m*/,0/*v*/,0.9/*beta1_power*/,0.999/*beta2_power*/,alpha/*lr == learning rate*/,
+                                         0.9/*beta1*/,0.999/*beta2*/,1e-6/*epsilon*/, 0.001 /*grad*/);
+
+    tf::ClientSession session(scope);
+    //TF_CHECK_OK(session.Run({{"x", x}}, {"train"}, nullptr));
+    return 0;
+
+}
