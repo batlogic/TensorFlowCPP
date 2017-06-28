8f443ddd3ecb5cafd66562e1d27f842106463769
Dror Meirovich <dror@drorm.com>
2017-06-28 17:38:51 +0300

    run assignments during creation of linear layer

--- train_example.cc
+++ train_example.cc
@@ -7,14 +7,17 @@ namespace tf {
     using namespace ::tensorflow;
     using namespace ::tensorflow::ops;
 
-    tf::Output FullyConnectedLinear(const tf::Scope& scope, tf::Input inp, const int sizeOutput) {
+    tf::Output FullyConnectedLinear(tf::ClientSession& session, const tf::Scope& scope, tf::Input inp, const int sizeOutput) {
+        std::vector<tf::Output> assignments;
         const int size_input = inp.tensor().shape().dim_size(1); // dim_size(1) == number of columns
         auto tf_dt   = inp.data_type();
         auto weights = tf::Variable(scope, {size_input,sizeOutput}, tf_dt);
-        tf::Assign(scope, weights, tf::TruncatedNormal(scope, weights, tf_dt));
+        assignments.push_back(tf::Assign(scope, weights, tf::TruncatedNormal(scope, weights, tf_dt)));
         auto biases  = tf::Variable(scope, {sizeOutput}, tf_dt);
-        tf::Assign(scope, biases, tf::ZerosLike(scope, biases));
-        return tf::Add (scope, tf::MatMul(scope, inp, weights), biases);
+        assignments.push_back(tf::Assign(scope, biases, tf::ZerosLike(scope, biases)));
+        auto result  = tf::Add (scope, tf::MatMul(scope, inp, weights), biases);
+        TF_CHECK_OK(session.Run(assignments, nullptr));
+        return result;
     }
 }
 
@@ -25,23 +28,23 @@ int main(int argc, char* argv[]) {
     const auto alpha   = 0.01; // learning rate
     tf::Scope    scope = tf::Scope::NewRootScope();
     tf::DataType tf_dt = tf::DT_DOUBLE;
+    tf::ClientSession session(scope);
     auto inputLinesPlaceHolder  = tf::Placeholder(scope.WithOpName("input"),  tf_dt, tf::Placeholder::Shape({-1,nin}) /*shape = (None,nin)*/);
     auto outputLinesPlaceHolder = tf::Placeholder(scope.WithOpName("output"), tf_dt, tf::Placeholder::Shape({-1,nout}) /*shape = (None,nout)*/);
     // Layer 1: Fully Connected. Input = nin. Output = nhidden.
-    auto fc1 = tf::FullyConnectedLinear(scope,inputLinesPlaceHolder,nhidden);
+    auto fc1 = tf::FullyConnectedLinear(session,scope,inputLinesPlaceHolder,nhidden);
 
     // activation of layer 1
     auto tanh1   = tf::Tanh(scope,fc1);
 
     // Layer 2: Fully Connected. Input = nhidden. Output = nout.
-    auto fc2 = tf::FullyConnectedLinear(scope,tanh1,nout);
+    auto fc2 = tf::FullyConnectedLinear(session,scope,tanh1,nout);
     // what should be the axis?
     auto cost_function = tf::Mean(scope,tf::SquaredDifference(scope,fc2,outputLinesPlaceHolder),1/*axis*/);
     auto train_operation = tf::ApplyAdam(scope.WithOpName("train"),cost_function,
                                          0/*m*/,0/*v*/,0.9/*beta1_power*/,0.999/*beta2_power*/,alpha/*lr == learning rate*/,
                                          0.9/*beta1*/,0.999/*beta2*/,1e-6/*epsilon*/, 0.001 /*grad*/);
 
-    tf::ClientSession session(scope);
     //TF_CHECK_OK(session.Run({{"x", x}}, {"train"}, nullptr));
     return 0;
 
